f527c22d96e1db2e8a3a48cf3e7b1ef18c00b080
Sigalas Alexandros <alxarch@gmail.com>
2016-05-12 19:09:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict';
+const DEFAULT_TTL = Infinity;
+
+class Cache extends Map {
+
+	constructor (ttl, interval) {
+		super();
+		this.ttl = ttl > 0 ? parseInt(ttl) : DEFAULT_TTL;
+		this.expires = new Map();
+		this.interval = interval > 0 ?
+			setInterval(this.trim.bind(this), interval) :
+			null;
+	}
+
+	now () {
+		return Date.now();
+	}
+
+	has (key) {
+		return super.has(key) && this.expires.get(key) > this.now();
+	}
+
+	delete (key) {
+		this.expires.delete(key);
+		return this.delete(key);
+	}
+
+	trim () {
+		const now = this.now();
+		let keys = new Set();
+		for (let pair of this.expires) {
+			if (pair[1] < now) {
+				keys.add(pair[0]);
+			}
+		}
+		for (let key of keys) {
+			super.delete(key);
+			this.expires.delete(key);
+		}
+		return keys;
+	}
+
+	check (key) {
+		return this.expires.get(key) > this.now();
+	}
+
+	get (key) {
+		return this.check(key) ? super.get(key) : void 0;
+	}
+
+	set (key, value, ttl) {
+		super.set(key, value);
+		ttl = ttl > 0 ? ttl : this.ttl;
+		this.expires.set(key, this.now() + ttl);
+		return this;
+	}
+
+}
+
+const mapiter = Map.prototype[Symbol.iterator];
+Cache.prototype.entries = function *cacheEntries() {
+	for (let pair of this) {
+		yield pair;
+	}
+	return;
+};
+Cache.prototype.keys = function *cacheKeys() {
+	for (let pair of this) {
+		yield pair[0];
+	}
+	return;
+};
+Cache.prototype.values = function *cacheValues() {
+	for (let pair of this) {
+		yield pair[1];
+	}
+	return;
+};
+Cache.prototype[Symbol.iterator] = function *cacheIterator() {
+	const now = this.now();
+	for (let pair of mapiter.apply(this)) {
+		if (this.expires.get(pair[0]) > now) {
+			yield pair;
+		}
+	}
+};
+
+module.exports = Cache;
---
+++ test.js
@@ -0,0 +1,129 @@
+'use strict';
+const co = require('co');
+const assert = require('assert');
+const Cache = require('.');
+const get = (map, key) => Map.prototype.get.call(map, key);
+describe('Cache', () => {
+	describe('Cache#keys()', () => {
+		it('Returns non expired keys', done => {
+			const c = new Cache(10);
+			const bar = {};
+			const baz = {};
+			c.set('foo', bar);
+			c.set('bar', baz, 30);
+			setTimeout(() => {
+				for (let key of c.keys()) {
+					assert.notEqual(key, 'foo', 'Skips expired keys');
+				}
+				done();
+
+			}, 20);
+		});
+	});
+	describe('Cache#values()', () => {
+		it('Returns non expired values', done => {
+			const c = new Cache(10);
+			const bar = {};
+			const baz = {};
+			c.set('foo', bar);
+			c.set('bar', baz, 30);
+			setTimeout(() => {
+				for (let value of c.values()) {
+					assert.notEqual(value, bar, 'Skips expired keys');
+				}
+				done();
+
+			}, 20);
+		});
+	});
+	describe('Cache#forEach()', () => {
+		it('Iterates through non expired keys', done => {
+			const c = new Cache(10);
+			const bar = {};
+			const baz = {};
+			c.set('foo', bar);
+			c.set('bar', baz, 30);
+			setTimeout(() => {
+				c.forEach((key, value) => {
+					assert.notEqual(key, 'foo', 'Skips expired keys');
+				});
+				done();
+
+			}, 20);
+		});
+	});
+	describe('Cache#@@iterator', () => {
+		it('Iterates through non expired keys', done => {
+			const c = new Cache(10);
+			const bar = {};
+			const baz = {};
+			c.set('foo', bar);
+			c.set('bar', baz, 30);
+			setTimeout(() => {
+				for (let pair of c) {
+					assert.notEqual(pair[0], 'foo', 'Skips expired keys');
+				}
+
+				done();
+
+			}, 20);
+		});
+	});
+
+	describe('new Cache()', () => {
+		it('Works with no arguments', () => {
+			const c = new Cache();
+			assert.strictEqual(c.ttl, Infinity);
+			assert.strictEqual(c.interval, null);
+		});
+		it('Is instance of Map', () => {
+			const c = new Cache();
+			assert.ok(c instanceof Map, 'Instance of Map');
+		});
+		it('Sets ttl', () => {
+			let c = new Cache(4);
+			assert.strictEqual(c.ttl, 4);
+			c = new Cache('7');
+			assert.strictEqual(c.ttl, 7);
+		});
+		it('Ignores invalid ttl', () => {
+			let c = new Cache(-4);
+			assert.equal(c.ttl, Infinity);
+			c = new Cache(null);
+			assert.equal(c.ttl, Infinity);
+			c = new Cache(undefined);
+			assert.equal(c.ttl, Infinity);
+			c = new Cache('foo');
+			assert.equal(c.ttl, Infinity);
+			c = new Cache({});
+			assert.equal(c.ttl, Infinity);
+		});
+
+	});
+	describe('Cache#set(key)', () => {
+		let c;
+		const ttl = 50;
+		const now = Date.now();
+		beforeEach(() => {
+			c = new Cache(ttl);
+			// Override now() to test values
+			c.now = () => now;
+		});
+		it('Caches a value', () => {
+			const bar = {};
+			c.set('foo', bar);
+			assert.strictEqual(get(c, 'foo'), bar);
+			assert.strictEqual(c.get('foo'), bar, 'Stores the value');
+			assert.strictEqual(get(c.expires, 'foo'), now + ttl);
+			return co( done => {
+				setTimeout(() => {
+					assert.strictEqual(c.has('foo'), false, 'Reports not having expired keys');
+					assert.strictEqual(c.get('foo'), undefined, 'Returns undefined for expired key');
+					assert.strictEqual(get(c, 'foo'), obj);
+					done();
+				}, ttl + 1)
+			});
+		});
+	});
+
+});
